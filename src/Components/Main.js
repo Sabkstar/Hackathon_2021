ad949daa08280c69330b39751d79a253fc4bdd1c
Khai Nguyen <khainl1110@gmail.com>
2021-01-09 22:43:05 -0800

    New design

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -15,14 +15,15 @@ export default function Main(props){
                 <p className = "line"></p>
             </div>
             <LeftNavBar/>
-
+            <div className = "bg-green">
+            </div>
             <Canvas>
                 <ambientLight intensity={0.5} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
                 <pointLight position={[-10, -10, -10]} />
                 <Box position={[-1.2, 0, 0]} />
                 <Box position={[2.5, 0, 0]} />
-            </Canvas>  
+            </Canvas> 
         </div>
 
     )
